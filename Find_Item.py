2fbe7ed3130b81ec6b7bae9b8487c70ac706cfee
NurbekSakiev <sakin.snn@gmail.com>
2015-05-05 23:33:51 -0500

    update

--- Find_Item.py
+++ Find_Item.py
@@ -1,1 +1,10 @@
 def findNode(self, node, val):
+	if (node is None):
+		return False
+	elif (node == node.val):
+		return True
+	elif (node < node.val):
+		return self.findNode(nodeLeftChild, val)
+	else:
+		return self.findNode(nodeRightChild, val)
+
